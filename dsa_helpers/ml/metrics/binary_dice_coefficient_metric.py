d501da72d5ddf3aeb8c2adc57ea46e73b2acab6b
JC Vizcarra <jvizcar@emory.edu>
2024-10-01 16:19:53 -0400

    version 0.4

--- dsa_helpers/ml/metrics/binary_dice_coefficient_metric.py
+++ dsa_helpers/ml/metrics/binary_dice_coefficient_metric.py
@@ -7,7 +7,8 @@ def binary_dice_coefficient_metric(eval_pred):
     with torch.no_grad():
         logits, labels = eval_pred  # assumed both are numpy
 
-        logits_tensor = torch.from_numpy(logits)
+        logits_tensor = torch.from_numpy(logits).cpu()
+        labels_tensor = torch.from_numpy(labels).cpu()
 
         # Scale the logits to the size of the label.
         logits_tensor = nn.functional.interpolate(
@@ -17,6 +18,7 @@ def binary_dice_coefficient_metric(eval_pred):
             align_corners=False,
         ).argmax(dim=1)
 
-        pred_labels = logits_tensor.detach().cpu().numpy()
+        pred_labels = logits_tensor.detach().numpy()
+        labels = labels_tensor.numpy()
 
         return {"dice_coefficient": binary_dice_coefficient(pred_labels, labels)}
