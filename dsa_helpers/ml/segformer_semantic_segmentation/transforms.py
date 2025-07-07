691238d783f1af73b3585c16714fba72c99a5765
JC Vizcarra <jvizcar@emory.edu>
2025-07-07 12:38:19 -0400

    not working yet

--- dsa_helpers/ml/segformer_semantic_segmentation/transforms.py
+++ dsa_helpers/ml/segformer_semantic_segmentation/transforms.py
@@ -1,19 +1,7 @@
 from transformers import SegformerImageProcessor
 from torchvision.transforms import ColorJitter
-
-
-def val_transforms_extras(example_batch):
-    """Default transforms for validation images with extras."""
-    processor = SegformerImageProcessor()
-
-    images = [x for x in example_batch["pixel_values"]]
-    labels = [x for x in example_batch["label"]]
-    inputs = processor(images, labels)
-    inputs["x"] = example_batch["x"]
-    inputs["y"] = example_batch["y"]
-    inputs["group"] = example_batch["group"]
-
-    return inputs
+import albumentations as A
+from typing import Callable
 
 
 def val_transforms(example_batch):
@@ -24,25 +12,6 @@ def val_transforms(example_batch):
     labels = [x for x in example_batch["label"]]
 
     inputs = processor(images, labels)
-
-    return inputs
-
-
-def train_transforms_extras(example_batch):
-    """Default transforms for training images with extras."""
-    processor = (
-        SegformerImageProcessor()
-    )  # required for using SegFormer model.
-    jitter = ColorJitter(
-        brightness=0.25, contrast=0.25, saturation=0.25, hue=0.1
-    )
-
-    images = [jitter(x) for x in example_batch["pixel_values"]]
-    labels = [x for x in example_batch["label"]]
-    inputs = processor(images, labels)
-    inputs["x"] = example_batch["x"]
-    inputs["y"] = example_batch["y"]
-    inputs["group"] = example_batch["group"]
 
     return inputs
 
@@ -61,3 +30,23 @@ def train_transforms(example_batch):
     inputs = processor(images, labels)
 
     return inputs
+
+
+def get_train_transforms(
+    square_symmetry_prob: float | None = 1.,
+    rotate_limit: int = 45,
+    rotate_prob: float | None = 0.5
+    rotate_fill: int
+) -> Callable:
+    """Get a train transform function that can be used to transform a
+    batch of images and labels. Used with batches for segformer
+    semantic segmentation models.
+
+    Args:
+
+    """
+    return A.Compose(
+        [
+            A.RandomBrightnessContrast(p=0.5),
+        ]
+    )
