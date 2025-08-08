1137e8f9a709c4185c805f740be34cb7bff2d814
JC Vizcarra <jvizcar@emory.edu>
2025-08-08 15:18:22 +0000

    update the versions

--- dsa_helpers/__init__.py
+++ dsa_helpers/__init__.py
@@ -3,7 +3,7 @@ from .imread import imread
 from .imwrite import imwrite
 
 # Version of the dsa-helpers package
-__version__ = "1.1.16"
+__version__ = "1.1.17dev0"
 
 # To avoid slow downs, do not allow from dsa_helpers import * to import anything.
 __all__ = []
