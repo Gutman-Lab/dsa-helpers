a3094291a9ea8652db311d66d147ff679dab85d5
candano <candano@protonmail.com>
2025-11-07 06:09:29 +0000

    added model_id, model_name to girder_utils for annot push

--- dsa_helpers/girder_utils.py
+++ dsa_helpers/girder_utils.py
@@ -523,6 +523,8 @@ def get_roi_with_yolo_labels_from_single_doc(
 def post_annotations_from_gdf(
     gc: GirderClient,
     item_id: str,
+    model_id: str,
+    model_name: str,
     doc_name: str,
     gdf: gpd.GeoDataFrame,
     idx_config: dict,
@@ -585,7 +587,7 @@ def post_annotations_from_gdf(
 
     response = gc.post(
         "/annotation",
-        parameters={"itemId": item_id},
+        parameters={"itemId": item_id, "modelId": model_id, "modelName": model_name},
         json={"name": doc_name, "description": "", "elements": elements},
     )
 
